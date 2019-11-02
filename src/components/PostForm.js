bca410682b947dc637d99823b460c83be26f0b17
Timothy Sharkey <Techne3@gmail.com>
2019-11-02 13:48:05 -0600

    Fixed editing bug now rerenders the edit

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,117 +1,108 @@
-import React,{useState, useEffect} from 'react';
+
+  
+import React, { useEffect, useState, Fragment } from 'react';
 import axios from 'axios';
 
-const PostForm = ({addPost, editingPost},props) => {
-    const [loading, setLoading]=useState(false)
-    const [errors, setErrors]=useState({})
-    const [post, setPost]=useState({
-        title: '',
-        body: '',
-    })
+function PostForm({ addPost, editingPost }) {
+  const [loading, setLoading] = useState(false);
+  const [post, setPost] = useState({ title: '', body: '' });
+  const [errors, setErrors] = useState({});
 
-    function validateForm(){
-        const tempErrors = {};
-        if(post.title.trim()=== ''){
-            tempErrors.title= 'title must not be empty'
-        }
-        if(post.body.trim()=== ''){
-            tempErrors.body= 'body must not be empty'
-        }
-        if(Object.keys(tempErrors).length > 0){
-            setErrors(tempErrors)
-            return(false)
-        }
-        return true
+  useEffect(() => {
+    setPost(editingPost);
+  }, [editingPost]);
+
+  const onChange = (event) => {
+    setPost({ ...post, [event.target.name]: event.target.value });
+  };
+
+  function validateForm() {
+    const tempErrors = {};
+    if (post.title.trim() === '') {
+      tempErrors.title = 'Title must not be empty';
+    }
+    if (post.body.trim() === '') {
+      tempErrors.body = 'Body must not be empty';
     }
 
-    useEffect(()=> {
-        setPost(editingPost)
-    },[editingPost])
+    if (Object.keys(tempErrors).length > 0) {
+      setErrors(tempErrors);
+      return false;
+    }
+    return true;
+  }
 
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setLoading(true);
 
-
-
-    const onChange= (e) => {
-        setPost({...post, [e.target.name]: e.target.value})
+    if (!validateForm()) {
+      setLoading(false);
+      return;
     }
 
-    const onSubmit = e => {
-        e.preventDefault();
-        setLoading(true)
+    setErrors({});
 
-        ///validate the form 
-        if(!validateForm()){
-            setLoading(false)
-            return 
-        }
-        
-        if(post.id){
-            axios
-            .put(`/post/${post.id}`,post)
-            .then(res=> {
-                console.log(res.data)
-                addPost(res.data)
-                setPost({
-                    title:"",
-                    body: ""
-                })
-                setLoading(false)
-            })
-            .catch(err=> console.log(err))
-
-        }else{
-            axios
-            .post('/post', post)
-            .then(res=> {
-                console.log(res.data)
-                addPost(res.data)
-                setPost({
-                    title:"",
-                    body: ""
-                })
-                setLoading(false)
-            })
-            .catch(err=> console.log(err))
-        }
+    if (post.id) {
+      axios
+        .put(`/post/${post.id}`, post)
+        .then((res) => {
+          addPost(res.data);
+          setPost({ title: '', body: '' });
+          setLoading(false);
+        })
+        .catch((err) => console.error(err));
+    } else {
+      axios
+        .post('/post', post)
+        .then((res) => {
+          addPost(res.data);
+          setPost({ title: '', body: '' });
+          setLoading(false);
+        })
+        .catch((err) => console.error(err));
     }
+  };
 
-
-
-    return (
-        <>
-         {!loading ? (
-             <form className="push-in" onSubmit={onSubmit}>
-                 <div className="input-field">
-                     <label htmlFor="title">Title</label>
-                     <input 
-                        type='text'
-                        name='title'
-                        value={post.title}
-                        onChange={onChange}
-                        className={errors.title && 'invalid'}
-                     />
-                     <span className="helper-text">{errors.title}</span>
-                    </div>
-                    <div className="input-field">
-                      <label htmlFor="body">Body</label> 
-                     <input 
-                        type='text'
-                        name='body'
-                        value={post.body}
-                        onChange={onChange}
-                        className={errors.body && 'invalid'}
-                     />
-                       <span className="helper-text">{errors.body}</span>
-                 </div>
-                 <button type="submit" className="waves-effect waves-light btn">{post.id? 'Update':'Add'}</button>
-             </form>
-         ):(
-             <div className="progress">
-                 <div className="indeterminate"></div>
-             </div>
-         )}   
-        </>
-    );
+  return (
+    <Fragment>
+      {!loading ? (
+        <form className="push-in" onSubmit={onSubmit}>
+          <div className="input-field">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              name="title"
+              value={post.title}
+              onChange={onChange}
+              className={errors.title && 'invalid'}
+            />
+            <span className="helper-text">{errors.title}</span>
+          </div>
+          <div className="input-field">
+            <label htmlFor="body">Body</label>
+            <input
+              type="text"
+              name="body"
+              value={post.body}
+              onChange={onChange}
+              className={errors.body && 'invalid'}
+            />
+            <span className="helper-text">{errors.body}</span>
+          </div>
+          <button type="submit" className="waves-effect waves-light btn">
+            {post.id ? 'Update' : 'Add'}
+          </button>
+        </form>
+      ) : (
+        <div className="progress">
+          <div className="indeterminate" />
+        </div>
+      )}
+    </Fragment>
+  );
 }
 
 export default PostForm;
+
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,95 +1,108 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import dayjs from 'dayjs';
+
 import PostForm from '../components/PostForm';
 
-const Home = (props) => {
-    const [ posts, setPosts ] = useState([]);
-    const [limit, setLimit]=useState(5)
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [limit, setLimit] = useState(5);
+  const [editingPost, setEditingPost] = useState({
+    title: '',
+    body: '',
+    id: null
+  });
 
-    const [editingPost, setEditingPost]= useState({
-        title: '',
-        body: '',
-        id: null,
-    })
+  useEffect(() => {
+    axios
+      .get('/posts')
+      .then((res) => {
+        console.log(res.data);
+        setPosts(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
-	useEffect(() => {
-		axios
-			.get('/posts')
-			.then((res) => {
-				console.log(res.data);
-				setPosts(res.data);
-			})
-			.catch((err) => console.log(err));
-    }, []);
+  function editPost(post) {
+    setEditingPost(post);
+  }
 
-    
-    const editPost = (whatever) => {
-        setEditingPost(whatever)
+  function deletePost(id) {
+    axios.delete(`/post/${id}`).then(() => {
+      const postsUpdated = posts.filter((p) => p.id !== id);
+      setPosts(postsUpdated);
+    });
+  }
+
+  function addPost(post) {
+    if (posts.find((p) => p.id === post.id)) {
+      const index = posts.findIndex((p) => p.id === post.id);
+      const postsUpdated = [...posts];
+      postsUpdated.splice(index, 1, post);
+      setPosts(postsUpdated);
+    } else {
+      const postsUpdated = [post, ...posts];
+      setPosts(postsUpdated);
     }
+  }
 
-    const deletePost = (id) => {
-       axios.delete(`/post/${id}`)
-       .then(()=>{
-           const postUpdated = posts.filter(p =>  p.id !== id)
-           setPosts(postUpdated)
-       })
-    }
+  const getNumberOfPosts = () => {
+    axios
+      .get(`/posts/${limit}`)
+      .then((res) => setPosts(res.data))
+      .catch((err) => console.error(err));
+  };
 
-    const addPost=(post)=>{
-        if(post.find(p => p.id === post.id)){
-            const index = post.findIndex(p => p.id === post.id)
-            const postsUpdated = [...posts]
-            postsUpdated.splice(index, 1,post)
-            setPosts(postsUpdated)
-        }else{
-            const postsUpdated=[post, ...posts];
-            setPosts(postsUpdated)
-        }
-    }
-
-
-    const getNumberOfPost =()=> {
-        axios.get(`/posts/${limit}`)
-        .then(res => setPosts(res.data))
-        .catch(err => console.log(err))
-    }
-
-
-
-	return (
-		<div>
-            <div className="row">
-                <div className="col s6">
-                    <PostForm addPost={addPost} editingPost={editingPost} />
-                </div>
-                <div className="col s3 push-in">
-                    <p>Limit number of post</p>
-                    <input type="number" 
-                        value={limit}
-                        onChange={e => setLimit(e.target.value)}
-                    />
-                    <button onClick={getNumberOfPost} className="waves-effect waves-light btn "> Set</button>
-                </div>
+  return (
+    <div>
+      <div className="row">
+        <div className="col s6">
+          <PostForm addPost={addPost} editingPost={editingPost} />
+        </div>
+        <div className="col s3 push-in">
+          <p>Limit number of posts</p>
+          <input
+            type="number"
+            value={limit}
+            onChange={(event) => setLimit(event.target.value)}
+          />
+          <button
+            onClick={getNumberOfPosts}
+            className="waves-effect waves-light btn"
+          >
+            Set
+          </button>
+        </div>
+      </div>
+      <div className="row">
+        {posts.map((post) => (
+          <div className="col s6" key={post.id}>
+            <div className="card">
+              <div className="card-content">
+                <div className="card-title">{post.title}</div>
+                <p className="timestamp">
+                  {dayjs(post.createdAt).format('MMM DD YYYY HH:mm')}
+                </p>
+                <p>{post.body}</p>
+              </div>
+              <div className="card-action">
+                <a href="#" onClick={editPost.bind(null, post)}>
+                  Edit
+                </a>
+                <a
+                  href="#"
+                  onClick={deletePost.bind(null, post.id)}
+                  className="delete-btn"
+                >
+                  Delete
+                </a>
+              </div>
             </div>
-			<div className="row">
-				{posts.map((post) => (
-					<div className="col s6">
-						<div className="card">
-							<div className="card-content">
-								<div className="card-title">{post.title}</div>
-								<p className="timestamp">{post.createdAt}</p>
-								<p>{post.body}</p>
-							</div>
-                            <div className="card-action">
-                                <a href="#" onClick={editPost.bind(null, post)} >Edit</a>
-                                <a href="#" onClick={deletePost.bind(null, post.id)}className="delete-btn">Delete</a>
-                            </div>
-						</div>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
